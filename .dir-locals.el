642dac33d777f8779deab6ffe1133f93bb4aca50
Ayush Kumar <266145228+nix-ayush@users.noreply.github.com>
2026-08-22 18:46:45 +0530

    Refactor: Automatic compile and run after saving a c++ file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,9 @@
-((c++-mode . ((eval . (setq compile-command
-                            (format "clang++ -std=c++20 -DONLINE_JUDGE -I.. %s ../catch_amalgamated.cpp -o solutions && ./solutions"
-                                    (file-name-nondirectory (or buffer-file-name ""))))))))
+;; ((c++-mode . ((eval . (setq compile-command
+                            ;; (format "clang++ -std=c++20 -DONLINE_JUDGE -I.. %s ../catch_amalgamated.cpp -o solutions && ./solutions"
+                                    ;; (file-name-nondirectory (or buffer-file-name ""))))))))
+
+;; auto-compile on save
+((c++-mode . ((eval . (add-hook 'after-save-hook
+                                (lambda ()
+                                  (compile "clang++ -std=c++20 -DONLINE_JUDGE -I.. remove_duplicates_from_sorted_array_26.cpp ../catch_amalgamated.cpp -o solutions && ./solutions"))
+                                nil t)))))
