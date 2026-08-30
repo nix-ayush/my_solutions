c38008c928874db9bd997aa74b6df0cccbbcc9e6
Ayush Kumar <266145228+nix-ayush@users.noreply.github.com>
2026-08-30 18:19:37 +0530

    Fix: Fixed auto save cmd error. Change so it automatically runs and sets local compile command to the edited file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,9 @@
 
 ;; auto-compile on save
 ((c++-mode . ((eval . (add-hook 'after-save-hook
-                                (lambda ()
-                                  (compile "clang++ -std=c++20 -DONLINE_JUDGE -I.. remove_duplicates_from_sorted_array_26.cpp ../catch_amalgamated.cpp -o solutions && ./solutions"))
+                                `(lambda ()
+                                   (setq-local compile-command
+                                               ,(format "clang++ -std=c++20 -DONLINE_JUDGE -I.. %s ../catch_amalgamated.cpp -o solutions && ./solutions"
+                                                        (file-name-nondirectory (or buffer-file-name ""))))
+                                   (compile compile-command))
                                 nil t)))))
